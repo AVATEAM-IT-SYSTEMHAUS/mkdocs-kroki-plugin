c14f5f7600336f2a8f32c36f080159de5ec27144
oniboni <oniboni@mailbox.org>
2024-05-16 09:38:12 +0000

    test: move tests out of classes again
    adds additional layer of complexity and does not solve anything

--- tests/test_errors_fail_fast.py
+++ tests/test_errors_fail_fast.py
@@ -3,50 +3,52 @@ import pytest
 from tests.utils import MkDocsHelper
 
 
-class TestErrorsFailFast:
-    @pytest.mark.usefixtures("kroki_timeout")
-    def test_request_timeout(self) -> None:
-        with MkDocsHelper("happy_path") as mkdocs_helper:
-            mkdocs_helper.set_http_method("POST")
-            mkdocs_helper.enable_fail_fast()
+@pytest.mark.usefixtures("kroki_timeout")
+def test_request_timeout() -> None:
+    with MkDocsHelper("happy_path") as mkdocs_helper:
+        mkdocs_helper.set_http_method("POST")
+        mkdocs_helper.enable_fail_fast()
 
-            result = mkdocs_helper.invoke_build()
+        result = mkdocs_helper.invoke_build()
 
-            assert result.exit_code == 1
-            assert "Request error" in result.output
-            assert "Aborted with a BuildError!" in result.output
+        assert result.exit_code == 1
+        assert "Request error" in result.output
+        assert "Aborted with a BuildError!" in result.output
 
-    @pytest.mark.usefixtures("kroki_bad_request")
-    def test_request_bad_request(self) -> None:
-        with MkDocsHelper("happy_path") as mkdocs_helper:
-            mkdocs_helper.set_http_method("POST")
-            mkdocs_helper.enable_fail_fast()
 
-            result = mkdocs_helper.invoke_build()
+@pytest.mark.usefixtures("kroki_bad_request")
+def test_request_bad_request() -> None:
+    with MkDocsHelper("happy_path") as mkdocs_helper:
+        mkdocs_helper.set_http_method("POST")
+        mkdocs_helper.enable_fail_fast()
 
-            assert result.exit_code == 1
-            assert "Diagram error!" in result.output
-            assert "Aborted with a BuildError!" in result.output
+        result = mkdocs_helper.invoke_build()
 
-    @pytest.mark.usefixtures("kroki_is_a_teapot")
-    def test_request_other_error(self) -> None:
-        with MkDocsHelper("happy_path") as mkdocs_helper:
-            mkdocs_helper.set_http_method("POST")
-            mkdocs_helper.enable_fail_fast()
+        assert result.exit_code == 1
+        assert "Diagram error!" in result.output
+        assert "Aborted with a BuildError!" in result.output
 
-            result = mkdocs_helper.invoke_build()
 
-            assert result.exit_code == 1
-            assert "Could not retrieve image data" in result.output
-            assert "Aborted with a BuildError!" in result.output
+@pytest.mark.usefixtures("kroki_is_a_teapot")
+def test_request_other_error() -> None:
+    with MkDocsHelper("happy_path") as mkdocs_helper:
+        mkdocs_helper.set_http_method("POST")
+        mkdocs_helper.enable_fail_fast()
 
-    @pytest.mark.usefixtures("kroki_dummy")
-    def test_missing_file_from(self) -> None:
-        with MkDocsHelper("missing_from_file") as mkdocs_helper:
-            mkdocs_helper.enable_fail_fast()
+        result = mkdocs_helper.invoke_build()
 
-            result = mkdocs_helper.invoke_build()
+        assert result.exit_code == 1
+        assert "Could not retrieve image data" in result.output
+        assert "Aborted with a BuildError!" in result.output
 
-            assert result.exit_code == 1
-            assert "kroki: Can't read file:" in result.output
-            assert "Aborted with a BuildError!" in result.output
+
+@pytest.mark.usefixtures("kroki_dummy")
+def test_missing_file_from() -> None:
+    with MkDocsHelper("missing_from_file") as mkdocs_helper:
+        mkdocs_helper.enable_fail_fast()
+
+        result = mkdocs_helper.invoke_build()
+
+        assert result.exit_code == 1
+        assert "kroki: Can't read file:" in result.output
+        assert "Aborted with a BuildError!" in result.output
